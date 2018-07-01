7f815a05d197f51c1003d8e5c61291b49abd0def
Mees van Dijk <mees@itisme.es>
2018-07-01 21:25:02 +0200

    added options in benchmark file and added warning end-users can now specify options from within the file with the other benchmark settings also now warns and exits when running in node < 10.5.0

--- src/bin.js
+++ src/bin.js
@@ -2,33 +2,53 @@
 const bench = require('./index.js')
 const path = require('path')
 const { performance } = require('perf_hooks')
+const semver = require('semver')
+
+if (!semver.gte(process.version, '10.5.0')) {
+  console.error('At least Node 10.5.0 is required, please update')
+  process.exit(1)
+}
 
 const start = performance.now()
+
+const defaultOptions = {
+  runs: 1000,
+  loops: 50,
+  // maxThreads: the amount of cores, is halved if ht is true to avoid hyperthreading,
+  ht: true,
+}
 
 const filename = path.resolve(
   /node/.test(process.argv[0]) ? process.argv[2] : process.argv[1],
 )
 ;(async () => {
-  const results = await bench(filename, {
-    runs: 1000,
-    loops: 50,
-    // maxThreads: 1,
-    ht: true,
-  })
-  const avg =
+  const options = Object.assign(
+    {},
+    defaultOptions,
+    require(filename).options || {},
+  )
+
+  const results = await bench(filename, options)
+
+  const stats = {}
+  stats.start = start
+  stats.end = performance.now()
+  stats.avg =
     results.reduce((total, current) => total + current) / results.length
-  const stddev = Math.sqrt(
+  stats.stddev = Math.sqrt(
     results
-      .map(entry => entry - avg)
+      .map(entry => entry - stats.avg)
       .map(diff => Math.pow(diff, 2))
       .reduce((total, current) => total + current) / results.length,
   )
-  const max = Math.max(...results)
-  const min = Math.min(...results)
-  const end = performance.now()
-  console.log(`avg:\t\t${avg.toFixed(3)}ms`)
-  console.log(`stddev:\t\t${stddev.toFixed(3)}ms`)
-  console.log(`max:\t\t${max.toFixed(3)}ms`)
-  console.log(`min:\t\t${min.toFixed(3)}`)
-  console.log(`total time:\t${(Math.floor(end - start) / 1000).toFixed(2)}s`)
+  stats.max = Math.max(...results)
+  stats.min = Math.min(...results)
+  stats.totalTime = stats.end - stats.start
+
+  console.log(`\
+avg:           ${stats.avg.toFixed(3)}ms
+stddev:        ${stats.stddev.toFixed(3)}ms
+max:           ${stats.max.toFixed(3)}ms
+min:           ${stats.min.toFixed(3)}ms
+total time:    ${(stats.totalTime / 1000).toFixed(2)}s`)
 })()
--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 module.exports = {
   benchmarkFunction() {
     for (let i = 0; i < Math.pow(10, 6); i++);
-  }
+  },
+  options: {
+    runs: 420,
+    loops: 50,
+  },
 }
