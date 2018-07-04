69ab64e066054079cf2991aa1aae150359ad491e
Mees van Dijk <mees@itisme.es>
2018-07-04 07:54:05 +0200

    Convert to from perf_hooks to process.hrtime fixes #1

--- src/worker.js
+++ src/worker.js
@@ -1,33 +1,30 @@
 const { parentPort, workerData } = require('worker_threads')
-const { PerformanceObserver, performance } = require('perf_hooks')
+
+function convertHrtime([seconds, nanoseconds]) {
+  return seconds * 1000 + nanoseconds / 1e6
+}
 
 const subject = require(workerData.filename)
 ;(async () => {
   const context = (subject.createContext && subject.createContext()) || {}
 
   const performanceEntries = []
-  const observer = new PerformanceObserver(list => {
-    performanceEntries.push(...list.getEntries().map(entry => entry.duration))
-  })
-  observer.observe({ entryTypes: ['measure'] })
   workerData.beforeBench && workerData.beforeBench(context)
 
   for (let i = 0; i < workerData.runs; i++) {
     subject.beforeRun && subject.beforeRun(context)
-    performance.mark(`run-${i}-start`)
+    const runStart = process.hrtime()
 
     for (let j = 0; j < workerData.loops; j++) {
       await subject.benchmarkFunction(context)
     }
 
-    performance.mark(`run-${i}-end`)
-    performance.measure(`run-${i}`, `run-${i}-start`, `run-${i}-end`)
-    performance.clearMarks()
+    const runTime = convertHrtime(process.hrtime(runStart))
+    performanceEntries.push(runTime)
     subject.afterRun && subject.afterRun(context)
   }
 
   subject.afterBench && subject.afterBench(context)
-  observer.disconnect()
 
   parentPort.postMessage({ performanceEntries })
   parentPort.unref()
